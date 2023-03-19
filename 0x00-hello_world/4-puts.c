3061bd7a7b0c03d652f0e5f3f0adacd8a5012139
Suleiman Ismail <suleimanismailsuleiman377@gmail.com>
2023-03-19 14:53:33 -0700

    some codes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/**
+*The main function
+*the return(0)
+*/
+#include <stdio.h>
+int main(void)
+{                                                                       puts("\"Programming is like building a multilingual puzzle \n");
+        return(0);
+}
